14174fb2e88021554e4ce9cae5503ef40d97b13d
username <youremail@domain.com>
2022-08-12 15:51:12 -0700

    district info showing

--- controllers/districts.js
+++ controllers/districts.js
@@ -1,9 +1,21 @@
+const district = require('../models/district');
 const District = require('../models/district');
 
 module.exports = {
-    new: newDistrict
+    new: newDistrict,
+    create,
 }
 
 function newDistrict(req, res){
     res.render('districts/new.ejs')
+}
+
+function create(req, res) {
+    console.log(req.body, '<-new district')
+    District.create(req.body, function(err, districtDocuments) {
+        if(err) {
+            console.log(err, '<-err in create controller');
+            return res.render('districts/new.ejs');
+        }
+    })
 }
--- routes/districts.js
+++ routes/districts.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const districtController = require('../controllers/districts');
 
 router.get('/new', districtController.new);
+router.post('/new', districtController.create);
 
 module.exports = router; 
