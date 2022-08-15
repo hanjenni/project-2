f9d24c0f590aa847267f8dd5dc24df14f3ada254
username <youremail@domain.com>
2022-08-15 16:26:16 -0700

    css district box

--- models/district.js
+++ models/district.js
@@ -2,21 +2,25 @@ const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
     content: String,
-    comRating: {type: Number, min: 1, max: 5},
-    adminRating: {type: Number, min: 1, max: 5},
-    workRating: {type: Number, min: 1, max: 5},
+    comRating: {
+        type: Number, min: 1, max: 5
+    },
+    adminRating: {
+        type: Number, min: 1, max: 5
+    },
+    workRating: {
+        type: Number, min: 1, max: 5
+    },
 },
 {
     timestamps: true,
 });
 
 
-
-
 const districtSchema = new mongoose.Schema({
-    districPic : {
-        type: String
-    },
+    //  districPic : {
+    //      type: String
+    // },
     districtName: {
         type: String,
         required: true,
