a002f1914c6948bf976720ac7f2337737ddb900a
username <youremail@domain.com>
2022-08-18 13:31:58 -0700

    code format

--- controllers/districts.js
+++ controllers/districts.js
@@ -1,4 +1,3 @@
-//const district = require('../models/district');
 const district = require('../models/district');
 const District = require('../models/district');
 
@@ -10,33 +9,33 @@ module.exports = {
 }
 
 function show(req, res) {
-    District.findById(req.params.id, function(err, districtInfo) {
-        res.render('districts/show.ejs', {title: 'District Details', districtInfo})
+    District.findById(req.params.id, function (err, districtInfo) {
+        res.render('districts/show.ejs', { title: 'District Details', districtInfo })
         console.log(districtInfo, '<-show all')
     });
 }
 
-function index(req, res){
-    District.find({}, function(err, districtDocuments){
+function index(req, res) {
+    District.find({}, function (err, districtDocuments) {
         console.log(districtDocuments, '<-all districts')
-        if (err){
+        if (err) {
             res.send('error finding the districts')
         }
         res.render('districts/index.ejs', {
             districts: districtDocuments
         });
-    });   
+    });
 }
 
-function newDistrict(req, res){
+function newDistrict(req, res) {
     res.render('districts/new.ejs')
 }
 
 function create(req, res) {
     console.log(req.body, '<-new district')
-    District.create(req.body, function(err, districtDocuments) {
-        if(err) {
-            console.log(err, '<-err in create controller');   
+    District.create(req.body, function (err, districtDocuments) {
+        if (err) {
+            console.log(err, '<-err in create controller');
         }
         res.redirect('/districts');
         console.log(districtDocuments, '<-added to db')
--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,53 +1,53 @@
 const District = require('../models/district');
 
 module.exports = {
-    create,
-    delete: deletePost,
-    edit,
+  create,
+  delete: deletePost,
+  edit,
 }
 
 async function edit(req, res) {
-console.log(req.body, '<-edits')
-const districtDoc = await District.findById(req.params.districtId)
-console.log(districtDoc, '<-district doc')
-const review = districtDoc.reviews.id(req.params.reviewId)
-console.log(review)
-review.content = req.body.content;
-review.comRating = req.body.comRating;
-review.adminRating = req.body.adminRating;
-review.workRating = req.body.workRating;
-districtDoc.save(function(err){ 
-  res.redirect(`/districts/${districtDoc._id}`)
-})
-  
+  console.log(req.body, '<-edits')
+  const districtDoc = await District.findById(req.params.districtId)
+  console.log(districtDoc, '<-district doc')
+  const review = districtDoc.reviews.id(req.params.reviewId)
+  console.log(review)
+  review.content = req.body.content;
+  review.comRating = req.body.comRating;
+  review.adminRating = req.body.adminRating;
+  review.workRating = req.body.workRating;
+  districtDoc.save(function (err) {
+    res.redirect(`/districts/${districtDoc._id}`)
+  })
+
 }
 
 
 function deletePost(req, res, next) {
-    District.findOne({'reviews._id': req.params.id, 'reviews.user': req.user._id}).then(function(districtInfo) {
-      if (!districtInfo) return res.redirect('/districts');
-      districtInfo.reviews.remove(req.params.id);
-      districtInfo.save().then(function() {
-        res.redirect(`/districts/${districtInfo._id}`);
-      }).catch(function(err) {
-        return next(err);
-      });
+  District.findOne({ 'reviews._id': req.params.id, 'reviews.user': req.user._id }).then(function (districtInfo) {
+    if (!districtInfo) return res.redirect('/districts');
+    districtInfo.reviews.remove(req.params.id);
+    districtInfo.save().then(function () {
+      res.redirect(`/districts/${districtInfo._id}`);
+    }).catch(function (err) {
+      return next(err);
     });
-  }
-  
-  
-function create(req, res){
-    console.log(req.params.id, '<-params district id')
-    console.log(req.body, '<-content of review')
-    District.findById(req.params.id, function(err, districtDocuments){
-        req.body.user = req.user._id;
-        req.body.userName = req.user.name;
-        req.body.userAvatar = req.user.avatar;
-        districtDocuments.reviews.push(req.body);
-        districtDocuments.save(function(err){
-            res.redirect(`/districts/${req.params.id}`)
-        })
-      
+  });
+}
+
+
+function create(req, res) {
+  console.log(req.params.id, '<-params district id')
+  console.log(req.body, '<-content of review')
+  District.findById(req.params.id, function (err, districtDocuments) {
+    req.body.user = req.user._id;
+    req.body.userName = req.user.name;
+    req.body.userAvatar = req.user.avatar;
+    districtDocuments.reviews.push(req.body);
+    districtDocuments.save(function (err) {
+      res.redirect(`/districts/${req.params.id}`)
     })
-    
+
+  })
+
 }
