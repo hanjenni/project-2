432c34e69dd644014eb65457bb7c70d4b3b4f594
username <youremail@domain.com>
2022-08-19 09:09:31 -0700

    isLoggedIn routes

--- routes/districts.js
+++ routes/districts.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const districtController = require('../controllers/districts');
+const isLoggedIn = require('../config/auth');
 
-router.get('/', districtController.index);
-router.get('/new', districtController.new);
-router.post('/', districtController.create);
-router.get('/:id', districtController.show);
+router.get('/', isLoggedIn, districtController.index);
+router.get('/new', isLoggedIn, districtController.new);
+router.post('/', isLoggedIn,districtController.create);
+router.get('/:id',isLoggedIn, districtController.show);
 
 module.exports = router; 
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const reviewsController = require('../controllers/reviews');
+const isLoggedIn = require('../config/auth');
 
-router.post('/districts/:id/reviews', reviewsController.create);
-router.delete('/reviews/:id', reviewsController.delete);
-
-router.put('/districts/:districtId/reviews/:reviewId', reviewsController.edit);
+router.post('/districts/:id/reviews', isLoggedIn, reviewsController.create);
+router.delete('/reviews/:id', isLoggedIn, reviewsController.delete);
+router.put('/districts/:districtId/reviews/:reviewId', isLoggedIn, reviewsController.edit);
 
 
 module.exports = router; 
